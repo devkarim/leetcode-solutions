e35b8b978f0bd8876b6fd751a780a61629ee5ed4
Karim <devkarim@hotmail.com>
2025-02-01 21:13:12 +0200

    fix: check for single element arr in problem 198

--- 198. House Robber/Solution198.java
+++ 198. House Robber/Solution198.java
@@ -10,6 +10,7 @@ class Solution198 {
 
 class Solution {
   public int rob(int[] nums) {
+    if (nums.length == 1) return nums[0];
     int[] dp = new int[nums.length];
     dp[0] = nums[0];
     dp[1] = Math.max(nums[0], nums[1]);
