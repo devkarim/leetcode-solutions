4bbe8f5c454adc372f8fb817dbd69fd2c0b18103
Karim <devkarim@hotmail.com>
2025-02-02 22:29:43 +0200

    refactor: solve problem 213 iterative

--- 213. House Robber II/Solution213.java
+++ 213. House Robber II/Solution213.java
@@ -1,17 +1,12 @@
-
-import java.util.Arrays;
-
 class Solution213 {
   public static void main(String[] args) {
     Solution sol = new Solution();
-    int[] nums = {1, 2, 1, 1};
+    int[] nums = {1, 2, 3};
     System.out.println(sol.rob(nums));    
   }
 }
 
 class Solution {
-  private int[] cache;
-
   public int rob(int[] nums) {
     // rules:
     // given: 0...n-1
@@ -19,19 +14,25 @@ class Solution {
     // rob either 0..n-2 or 1..n-1
     if (nums.length == 1) return nums[0];
     if (nums.length == 2) return Math.max(nums[0], nums[1]);
-    cache = new int[nums.length];
-    Arrays.fill(cache, -1);
     int n = nums.length;
-    int withFirstHouse = dp(nums, 0, n - 1);
-    Arrays.fill(cache, -1);
-    int withoutFirstHouse = dp(nums, 1, n);
-    return Math.max(withFirstHouse, withoutFirstHouse);
+    return Math.max(dp(nums, 0, n - 1), dp(nums, 1, n));
   }
 
-  private int dp(int[] nums, int idx, int n) {
-    if (idx >= n) return 0;
-    if (cache[idx] != -1) return cache[idx];
-    cache[idx] = Math.max(dp(nums, idx + 1, n), nums[idx] + dp(nums, idx + 2, n));
-    return cache[idx];
+  private int dp(int[] nums, int start, int n) {
+    int prevPrev = nums[start];
+    int prev = Math.max(nums[start], nums[start + 1]);
+    for (int i = start + 2; i < n; i++) {
+      int temp = prev;
+      prev = Math.max(prev, nums[i] + prevPrev);
+      prevPrev = temp;
+    }
+    return prev;
   }
+  
+  // private int dp(int[] nums, int idx, int n) {
+  //   if (idx >= n) return 0;
+  //   if (cache[idx] != -1) return cache[idx];
+  //   cache[idx] = Math.max(dp(nums, idx + 1, n), nums[idx] + dp(nums, idx + 2, n));
+  //   return cache[idx];
+  // }
 }
