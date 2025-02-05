fbcc7edd40b6df5ba704a33e9770ea0889af9c84
Karim <devkarim@hotmail.com>
2025-02-05 22:50:56 +0200

    feat: solve problem 322

---
+++ 322. Coin Change/Solution322.java
@@ -0,0 +1,41 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution322 {
+  public static void main(String[] args) {
+    Solution sol = new Solution();
+    // int[] coins = {1,2,5};
+    // int amount = 11;
+    int[] coins = {2,5,10,1};
+    int amount = 27;
+    System.out.println(sol.coinChange(coins, amount));
+    System.out.println(sol.cache);
+  }
+}
+
+class Solution {
+  public Map<Integer, Integer> cache;
+
+  public int coinChange(int[] coins, int amount) {
+    if (amount == 0) return 0;
+    cache = new HashMap<>();
+    int minCoins = dfs(coins, amount);
+    return Integer.MAX_VALUE == minCoins ? -1 : minCoins;
+  }
+
+  public int dfs(int[] coins, int amount) {
+    if (amount == 0) return 0;
+    if (cache.containsKey(amount)) return cache.get(amount);
+    int min = Integer.MAX_VALUE;
+    for (int coin : coins) {
+      if (amount - coin >= 0) {
+        int minCoins = dfs(coins, amount - coin);
+        if (minCoins != Integer.MAX_VALUE) {
+          min = Math.min(min, minCoins + 1);
+        }
+      }
+    }
+    cache.put(amount, min);
+    return min;
+  }
+}
