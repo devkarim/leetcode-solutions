8456fc885c978f7cf015e8ecdb86eb4efa6623e8
Karim <devkarim@hotmail.com>
2025-02-05 20:28:51 +0200

    refactor: remove subset from problem 91

--- 91. Decode Ways/Solution91.java
+++ 91. Decode Ways/Solution91.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 class Solution91 {
   public static void main(String[] args) {
@@ -12,7 +10,6 @@ class Solution91 {
 
 class Solution {
   private int[] cache;
-  private List<Character> subset = new ArrayList<>();
   
   public int numDecodings(String s) {
     if (s.length() == 1) return s.charAt(0) == '0' ? 0 : 1;
@@ -25,14 +22,10 @@ class Solution {
     if (idx >= s.length()) return 1;
     if (s.charAt(idx) == '0') return 0;
     if (cache[idx] != -1) return cache[idx];
-    subset.add(s.charAt(idx));
     int res = dp(s, idx + 1);
-    subset.removeLast();
     if (s.length() > idx + 1) {
       if (s.charAt(idx) == '1' || (s.charAt(idx) == '2' && s.charAt(idx + 1) < '7')) {
-        subset.add(s.charAt(idx + 2));
         res += dp(s, idx + 2);
-        subset.removeLast();
       }
     }
     cache[idx] = res;
