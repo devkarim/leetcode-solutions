7cb25359cd5457b258fcc92a3284b5282114f15e
Karim <devkarim@hotmail.com>
2025-02-11 22:24:46 +0200

    refactor: solve problem 300 iterative

--- 300. Longest Increasing Subsequence/Solution300.java
+++ 300. Longest Increasing Subsequence/Solution300.java
@@ -3,35 +3,24 @@ import java.util.Arrays;
 class Solution300 {
   public static void main(String[] args) {
     Solution sol = new Solution();
-    int[] nums = {4, 10, 4, 3, 8, 9};
+    //int[] nums = {4, 10, 4, 3, 8, 9};
+    int[] nums = {0, 1, 0, 3, 2, 3};
     System.out.println(sol.lengthOfLIS(nums));
   }
 }
 
 class Solution {
-  private int[][] cache;
-
   public int lengthOfLIS(int[] nums) {
-    int n = nums.length;
-    cache = new int[n][n + 1];
-    for (int[] row : cache) {
-      Arrays.fill(row, -1);
+    int[] dp = new int[nums.length];
+    Arrays.fill(dp, 1);
+    int res = 1;
+    for (int start = nums.length - 1; start >= 0; start--) {
+      for (int end = start + 1; end < nums.length; end++) {
+        if (nums[end] <= nums[start]) continue;
+        dp[start] = Math.max(dp[start], 1 + dp[end]);
+        res = Math.max(res, dp[start]);
+      }
     }
-    return dfs(nums, 0, -1);
-  }
-
-  private int dfs(int[] nums, int start, int last) {
-    if (start >= nums.length) {
-      return 0;
-    }
-    if (cache[start][last + 1] != -1) return cache[start][last + 1];
-    // exclude current item
-    int res = dfs(nums, start + 1, last);
-    // include current element if it is larger than last item
-    if (last == -1 || nums[last] < nums[start]) {
-      res = Math.max(res, 1 + dfs(nums, start + 1, start));
-    }
-    cache[start][last + 1] = res;
     return res;
   }
 }
