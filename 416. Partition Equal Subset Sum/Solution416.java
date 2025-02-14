46eadba5fa127ecb329d870df88f521ac4cc1b0b
Karim <devkarim@hotmail.com>
2025-02-14 22:55:07 +0200

    refactor: solve problem 416 iterative

--- 416. Partition Equal Subset Sum/Solution416.java
+++ 416. Partition Equal Subset Sum/Solution416.java
@@ -1,4 +1,5 @@
-import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution416 {
   public static void main(String[] args) {
@@ -10,27 +11,49 @@ class Solution416 {
 }
 
 class Solution {
-  private Boolean[][] cache;
-
   public boolean canPartition(int[] nums) {
     int sum = 0;
-    for (int n : nums) {
-      sum += n;
+    for (int num : nums) {
+      sum += num;
     }
     if (sum % 2 != 0) return false;
     int target = sum / 2;
-    cache = new Boolean[nums.length][target + 1];
-    return dfs(nums, 0, target);
-  }
-
-  private boolean dfs(int[] nums, int idx, int target) {
-    if (idx >= nums.length) {
-      return target == 0;
+    Set<Integer> dp = new HashSet<>();
+    dp.add(0);
+    for (int n : nums) {
+      Set<Integer> newDP = new HashSet<>();
+      for (int t : dp) {
+        newDP.add(n + t);
+        newDP.add(t);
+      }
+      dp = newDP;
     }
-    if (target < 0) return false;
-    if (cache[idx][target] != null) return cache[idx][target];
-    cache[idx][target] = dfs(nums, idx + 1, target) || dfs(nums, idx + 1, target - nums[idx]);
-    return cache[idx][target];
+    return dp.contains(target);
   }
 }
+
+// class Solution {
+//   private Boolean[][] cache;
+// 
+//   public boolean canPartition(int[] nums) {
+//     int sum = 0;
+//     for (int n : nums) {
+//       sum += n;
+//     }
+//     if (sum % 2 != 0) return false;
+//     int target = sum / 2;
+//     cache = new Boolean[nums.length][target + 1];
+//     return dfs(nums, 0, target);
+//   }
+// 
+//   private boolean dfs(int[] nums, int idx, int target) {
+//     if (idx >= nums.length) {
+//       return target == 0;
+//     }
+//     if (target < 0) return false;
+//     if (cache[idx][target] != null) return cache[idx][target];
+//     cache[idx][target] = dfs(nums, idx + 1, target) || dfs(nums, idx + 1, target - nums[idx]);
+//     return cache[idx][target];
+//   }
+// }
 
