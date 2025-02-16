79531b37e840d3f4cc7e4578308a27a319416ce6
Karim <devkarim@hotmail.com>
2025-02-16 22:00:23 +0200

    refactor: final step to conver recursive to iterative for problem 70

--- 70. Climbing Stairs/go/solution70.go
+++ 70. Climbing Stairs/go/solution70.go
@@ -43,15 +43,14 @@ f(n) = f(n-1) + f(n-2)
 */
 
 func climbStairs(n int) int {
-	dp := make([]int, n+2)
-	dp[0] = 1
-	dp[1] = 1
+	prevPrev := 1
+	prev := 1
 
-	for i := 2; i <= n; i++ {
-		dp[i] = dp[i-1] + dp[i-2]
+	for i := 2; i <= n+1; i++ {
+		prevPrev, prev = prev, prev+prevPrev
 	}
 
-	return dp[n]
+	return prevPrev
 }
 
 // 0 1 2 3 4
