cbdc0303826afe917be4aad2af708b8ef6f1d0b1
Karim <devkarim@hotmail.com>
2025-02-27 21:05:25 +0200

    refactor(463): solve using iterating through whole grid

--- 463. Island Perimeter/go/solution463.go
+++ 463. Island Perimeter/go/solution463.go
@@ -12,65 +12,32 @@ func main() {
 	fmt.Println(islandPerimeter(grid))
 }
 
-type cell struct {
-	x int
-	y int
-}
-
-type queue []cell
-
-func (q *queue) push(c cell) {
-	*q = append(*q, c)
-}
-
-func (q *queue) pop() cell {
-	c := (*q)[0]
-	*q = (*q)[1:]
-	return c
-}
-
 func islandPerimeter(grid [][]int) int {
 	rows := len(grid)    // number of rows
 	cols := len(grid[0]) // number of cols
 
-	visited := make([][]bool, rows)
-	for i := range visited {
-		visited[i] = make([]bool, cols)
+	perimeter := 0
+
+	directions := [][]int{
+		{0, 1},
+		{0, -1},
+		{1, 0},
+		{-1, 0},
 	}
 
-	for r := range grid {
-		for c := range grid[r] {
-			if grid[r][c] == 1 {
-				q := &queue{}
-				q.push(cell{x: c, y: r})
-				visited[r][c] = true
-				perimeter := 0
-
-				directions := [][]int{
-					{0, 1},
-					{1, 0},
-					{0, -1},
-					{-1, 0},
-				}
-
-				for len(*q) != 0 {
-					curr := q.pop()
-					for _, dir := range directions {
-						dx, dy := dir[0], dir[1]
-						nx, ny := curr.x+dx, curr.y+dy
-						if nx < 0 || ny < 0 || nx >= cols || ny >= rows || grid[ny][nx] == 0 {
-							perimeter++
-						} else if !visited[ny][nx] {
-							visited[ny][nx] = true
-							q.push(cell{x: nx, y: ny})
-						}
+	for y := range grid {
+		for x := range grid[y] {
+			if grid[y][x] == 1 {
+				for _, d := range directions {
+					dx, dy := d[0], d[1]
+					nx, ny := x+dx, y+dy
+					if nx < 0 || ny < 0 || nx >= cols || ny >= rows || grid[ny][nx] == 0 {
+						perimeter++
 					}
 				}
-
-				return perimeter
 			}
 		}
 	}
 
-	return -1
+	return perimeter
 }
