e72220ec59e31094225b0b2ab40fb725f682d98b
Karim <devkarim@hotmail.com>
2025-03-01 22:50:54 +0200

    refactor(63): final step to convert recursive to iterative(dp) solution

--- 63. Unique Paths II/go/solution63.go
+++ 63. Unique Paths II/go/solution63.go
@@ -15,22 +15,18 @@ func uniquePathsWithObstacles(obstacleGrid [][]int) int {
 	m := len(obstacleGrid)
 	n := len(obstacleGrid[0])
 
-	dp := make([][]int, m+1)
-	for i := range dp {
-		dp[i] = make([]int, n+1)
-	}
+	dp := make([]int, n+1)
+	dp[n-1] = 1
 
 	for i := m - 1; i >= 0; i-- {
 		for j := n - 1; j >= 0; j-- {
 			if obstacleGrid[i][j] == 1 {
-				dp[i][j] = 0
-			} else if i == m-1 && j == n-1 {
-				dp[i][j] = 1
+				dp[j] = 0
 			} else {
-				dp[i][j] = dp[i+1][j] + dp[i][j+1]
+				dp[j] += dp[j+1]
 			}
 		}
 	}
 
-	return dp[0][0]
+	return dp[0]
 }
