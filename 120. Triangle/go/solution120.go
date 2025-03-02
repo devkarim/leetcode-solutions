a22c5e7ec8ee8a2b1590db206be97bc1ec67ce0f
Karim <devkarim@hotmail.com>
2025-03-03 01:10:13 +0200

    feat(120): step 1 to convert recursive sol to iterative

--- 120. Triangle/go/solution120.go
+++ 120. Triangle/go/solution120.go
@@ -16,26 +16,20 @@ func minimumTotal(triangle [][]int) int {
 	m := len(triangle)
 	n := len(triangle[m-1])
 
-	cache := make([][]int, m)
-	for i := range cache {
-		cache[i] = make([]int, n)
-		for j := range cache[i] {
-			cache[i][j] = -1
+	dp := make([][]int, m+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+	}
+
+	dfs := func(i, j int) int {
+		return triangle[i][j] + min(dp[i+1][j+1], dp[i+1][j])
+	}
+
+	for i := m - 1; i >= 0; i-- {
+		for j := len(triangle[i]) - 1; j >= 0; j-- {
+			dp[i][j] = dfs(i, j)
 		}
 	}
 
-	var dfs func(i, j int) int
-
-	dfs = func(i, j int) int {
-		if i >= m || j >= i+1 {
-			return 0
-		}
-		if cache[i][j] != -1 {
-			return cache[i][j]
-		}
-		cache[i][j] = triangle[i][j] + min(dfs(i+1, j+1), dfs(i+1, j))
-		return cache[i][j]
-	}
-
-	return dfs(0, 0)
+	return dp[0][0]
 }
