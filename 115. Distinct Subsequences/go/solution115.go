f1384b6c85f73ac9c7b2c628306c12ac8dad6ab0
Karim <devkarim@hotmail.com>
2025-03-13 23:13:09 +0200

    refactor(115): solve using bottom-up sol

--- 115. Distinct Subsequences/go/solution115.go
+++ 115. Distinct Subsequences/go/solution115.go
@@ -12,37 +12,21 @@ func numDistinct(s string, t string) int {
 	m := len(s)
 	n := len(t)
 
-	cache := make([][]int, m)
-	for i := range cache {
-		cache[i] = make([]int, n)
-		for j := range cache[i] {
-			cache[i][j] = -1
+	dp := make([][]int, m+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+		dp[i][n] = 1
+	}
+
+	for i := m - 1; i >= 0; i-- {
+		for j := n - 1; j >= 0; j-- {
+			if s[i] == t[j] {
+				dp[i][j] = dp[i+1][j+1] + dp[i+1][j]
+			} else {
+				dp[i][j] = dp[i+1][j]
+			}
 		}
 	}
 
-	var dfs func(i, j int) int
-
-	dfs = func(i, j int) int {
-		// if reached the end of t then we found a match
-		if j >= n {
-			return 1
-		}
-		// if reached the end of s then we didn't find a match
-		if i >= m {
-			return 0
-		}
-		if cache[i][j] != -1 {
-			return cache[i][j]
-		}
-		// if both characters are equal, advance j and i OR just advance i to try other subsequences
-		if s[i] == t[j] {
-			cache[i][j] = dfs(i+1, j+1) + dfs(i+1, j)
-		} else {
-			// if both characters are not equal, advance only i since we didn't find a matching character
-			cache[i][j] = dfs(i+1, j)
-		}
-		return cache[i][j]
-	}
-
-	return dfs(0, 0)
+	return dp[0][0]
 }
