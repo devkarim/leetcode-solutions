3b35286ba3de8e0b2d29db88385ade935391de73
Karim <devkarim@hotmail.com>
2025-03-15 00:05:40 +0200

    refactor(300): solve using bottom-up sol

--- 300. Longest Increasing Subsequence/go/solution300.go
+++ 300. Longest Increasing Subsequence/go/solution300.go
@@ -20,33 +20,22 @@ func main() {
 func lengthOfLIS(nums []int) int {
 	n := len(nums)
 
-	cache := make([][]int, n)
-	for i := range cache {
-		cache[i] = make([]int, n)
-		for j := range cache[i] {
-			cache[i][j] = -1
+	dp := make([]int, n)
+	for i := range dp {
+		dp[i] = 1
+	}
+
+	res := 1
+
+	for start := n - 1; start >= 0; start-- {
+		for end := start + 1; end < n; end++ {
+			if nums[end] <= nums[start] {
+				continue
+			}
+			dp[start] = max(dp[start], 1+dp[end])
+			res = max(res, dp[start])
 		}
 	}
 
-	var dfs func(curr, last int) int
-
-	dfs = func(curr, last int) int {
-		if curr >= n {
-			return 0
-		}
-		if cache[curr][last+1] != -1 {
-			return cache[curr][last+1]
-		}
-		// exclude current element
-		res := dfs(curr+1, last)
-		// include current element if it is the first element or
-		// current item is larger than last item
-		if last == -1 || nums[last] < nums[curr] {
-			res = max(res, 1+dfs(curr+1, curr))
-		}
-		cache[curr][last+1] = res
-		return res
-	}
-
-	return dfs(0, -1)
+	return res
 }
