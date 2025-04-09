abce3f0f969e0dac5ee883cb19c7cde6a58a5ebc
Karim Wael <devkarim@hotmail.com>
2025-04-09 22:14:45 +0200

    refactor(518): final step to convert recursive to iterative solution

--- 518. Coin Change II/go/solution518.go
+++ 518. Coin Change II/go/solution518.go
@@ -10,38 +10,17 @@ func main() {
 
 func change(amount int, coins []int) int {
 	n := len(coins)
-	dp := make([][]int, n+1)
 
-	for i := range dp {
-		dp[i] = make([]int, amount+1)
-	}
-
-	dp[n][0] = 1
-
-	// for total := 0; total <= amount; total++ {
-	// 	for i := n - 1; i >= 0; i-- {
-	// 		take := 0
-	// 		if total >= coins[i] {
-	// 			take = dp[i][total-coins[i]]
-	// 		}
-	// 		skip := dp[i+1][total]
-	// 		dp[i][total] = skip + take
-	// 	}
-	// }
-
-	prev := make([]int, amount+1)
-	prev[0] = 1
+	dp := make([]int, amount+1)
+	dp[0] = 1
 
 	for i := n - 1; i >= 0; i-- {
-		dp := make([]int, amount+1)
 		for total := 0; total <= amount; total++ {
-			dp[total] = prev[total]
 			if total >= coins[i] {
 				dp[total] += dp[total-coins[i]]
 			}
 		}
-		prev = dp
 	}
 
-	return prev[amount]
+	return dp[amount]
 }
