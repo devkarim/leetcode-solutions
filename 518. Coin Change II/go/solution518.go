042ea2a44680e94c032efacc4b0166886ce41aae
Karim Wael <devkarim@hotmail.com>
2025-04-09 22:03:50 +0200

    refactor(518): step 3 to convert recursive to iterative solution

--- 518. Coin Change II/go/solution518.go
+++ 518. Coin Change II/go/solution518.go
@@ -10,7 +10,7 @@ func main() {
 
 func change(amount int, coins []int) int {
 	n := len(coins)
-	dp := make([][]int, n+2)
+	dp := make([][]int, n+1)
 
 	for i := range dp {
 		dp[i] = make([]int, amount+1)
@@ -18,16 +18,30 @@ func change(amount int, coins []int) int {
 
 	dp[n][0] = 1
 
-	for total := 0; total <= amount; total++ {
-		for i := n - 1; i >= 0; i-- {
-			take := 0
+	// for total := 0; total <= amount; total++ {
+	// 	for i := n - 1; i >= 0; i-- {
+	// 		take := 0
+	// 		if total >= coins[i] {
+	// 			take = dp[i][total-coins[i]]
+	// 		}
+	// 		skip := dp[i+1][total]
+	// 		dp[i][total] = skip + take
+	// 	}
+	// }
+
+	prev := make([]int, amount+1)
+	prev[0] = 1
+
+	for i := n - 1; i >= 0; i-- {
+		dp := make([]int, amount+1)
+		for total := 0; total <= amount; total++ {
+			dp[total] = prev[total]
 			if total >= coins[i] {
-				take = dp[i][total-coins[i]]
+				dp[total] += dp[total-coins[i]]
 			}
-			skip := dp[i+1][total]
-			dp[i][total] = skip + take
 		}
+		prev = dp
 	}
 
-	return dp[0][amount]
+	return prev[amount]
 }
