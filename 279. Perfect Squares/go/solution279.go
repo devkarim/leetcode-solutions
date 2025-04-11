65f39b73b0fcfda64e1e1faf45fcb523536bb7c3
Karim Wael <devkarim@hotmail.com>
2025-04-11 22:06:38 +0200

    feat: solve problem 279

---
+++ 279. Perfect Squares/go/solution279.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	n := 13
+	fmt.Println(numSquares(n))
+}
+
+func numSquares(n int) int {
+	cache := make([][]int, n+1)
+
+	for i := range cache {
+		cache[i] = make([]int, int(math.Sqrt(float64(n)))+1)
+		for j := range cache[i] {
+			cache[i][j] = -1
+		}
+	}
+
+	var dfs func(k, i int) int
+
+	dfs = func(k, i int) int {
+		if i > int(math.Sqrt(float64(k))) {
+			if k == 0 {
+				return 0
+			}
+			return n
+		}
+		if cache[k][i] != -1 {
+			return cache[k][i]
+		}
+		// take
+		take := 1 + dfs(k-i*i, i)
+		// skip
+		skip := dfs(k, i+1)
+		cache[k][i] = min(take, skip)
+		return cache[k][i]
+	}
+
+	return dfs(n, 1)
+}
