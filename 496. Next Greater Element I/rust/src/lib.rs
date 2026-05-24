a1e1518aba696bdea1a0b6ddd4b15789ada4f772
devkarim <devkarim@hotmail.com>
2026-05-24 17:42:44 +0300

    perf(496): solve using monotonic stack

--- 496. Next Greater Element I/rust/src/lib.rs
+++ 496. Next Greater Element I/rust/src/lib.rs
@@ -1,21 +1,27 @@
 #![allow(dead_code)]
 
+use std::collections::HashMap;
+
 struct Solution {}
 
 impl Solution {
     pub fn next_greater_element(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
-        let mut res = vec![-1; nums1.len()];
+        let mut res = Vec::with_capacity(nums1.len());
+        let mut num_greater_map = HashMap::new();
+        let mut monotonic_stack: Vec<i32> = Vec::with_capacity(nums2.len());
 
-        for (i, &n1) in nums1.iter().enumerate() {
-            let mut found = false;
-            for (_, &n2) in nums2.iter().enumerate() {
-                if n1 == n2 {
-                    found = true;
-                } else if found && n2 > n1 {
-                    res[i] = n2;
-                    break;
-                }
+        for &n in nums2.iter() {
+            while let Some(&top) = monotonic_stack.last()
+                && top < n
+            {
+                monotonic_stack.pop();
+                num_greater_map.insert(top, n);
             }
+            monotonic_stack.push(n);
+        }
+
+        for n in nums1 {
+            res.push(num_greater_map.get(&n).copied().unwrap_or(-1));
         }
 
         res
